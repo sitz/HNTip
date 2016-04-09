61f68714095fca07b5917534cea26f99a8d564c0
Sitesh Shrivastava <siteshshrivastava@gmail.com>
2016-04-09 20:50:36 +0530

    Keep track of state of events

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -6,43 +6,51 @@ chrome.runtime.onInstalled.addListener(details => {
   console.log('Installation Details: ', details);
 });
 
-window.onload = function checkHN() {
+var notificationData = {};
+
+chrome.notifications.onClosed.addListener(function (notificationId) {
+  delete notificationData[notificationId];
+});
+
+chrome.notifications.onClicked.addListener(function (notificationId) {
+  chrome.notifications.clear(notificationId);
+  chrome.tabs.create({
+    url: 'https://news.ycombinator.com/item?id=' + notificationData[notificationId].id
+  })
+});
+
+function createNotification(itemData) {
+  var itemDetails = itemData.val();
+  console.log(itemDetails);
+
+  if (itemDetails && itemDetails.hasOwnProperty('title') && itemDetails.hasOwnProperty('by')) {
+
+    chrome.notifications.create(
+      '', {
+        type: 'basic',
+        title: itemDetails.title,
+        message: 'by: ' + itemDetails.by,
+        iconUrl: '/images/icon-128.png'
+      }, function onCreation(createdId) {
+
+        notificationData[createdId] = itemDetails;
+
+      });
+  }
+}
+
+function checkHN() {
   var hn = new Firebase('https://hacker-news.firebaseio.com/v0/');
-  var notificationId;
 
   hn.child('maxitem').on('value', function (snapshot) {
     var itemId = snapshot.val();
     console.log('Id: ' + itemId);
 
-    hn.child('item/' + itemId).on('value', function (itemData) {
-      var itemDetails = itemData.val();
-      console.log(itemDetails);
-
-      if (itemDetails && itemDetails.hasOwnProperty('title') && itemDetails.hasOwnProperty('by')) {
-
-        var currentNotificationId = chrome.notifications.create(
-          '', {
-            type: 'basic',
-            title: itemDetails.title,
-            message: 'by: ' + itemDetails.by,
-            iconUrl: '/images/icon-128.png'
-          },
-          function onCreation(notificationId) {
-            console.log('notificationId: ' + notificationId);
-
-            chrome.notifications.onClicked.addListener(function (notificationId) {
-              if (currentNotificationId === notificationId) {
-                chrome.notifications.clear(notificationId);
-                chrome.tabs.create({'url': 'https://news.ycombinator.com/item?id=' + itemId});
-              }
-            });
-
-            console.log('Last error:', chrome.runtime.lastError);
-          });
-      }
-    });
+    hn.child('item/' + itemId).on('value', createNotification);
 
   }, function (errorObject) {
     console.log('The read failed: ' + errorObject.code);
   });
-};
+}
+
+window.onload = checkHN;
